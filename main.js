c76d6b8022f150ff495fd624f826f34a67553c99
Lenka Polcerova <lenka.polcerova@gmail.com>
2023-01-19 22:27:04 +0100

    splneni ukolu

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+function zmenBarvu(){
+  let ctverecek = document.querySelector('.ctverecek');
+  ctverecek.style.backgroundColor = "green";
+}
+
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
@@ -13,9 +17,18 @@ function secti(a, b) {
   return c;
 }
 
+function zobrazVysledek(){
+  let vysledek = document.querySelector("#vysledek").innerHTML = secti(4, 5);
+}
+
+
 /**
  * Upozorní uživatele při spuštění.
  */
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log("Gratulace, právě jsi spustila tuto funkci!");
+  ctverecek = document.querySelector('.ctverecek');
+  ctverecek.textContent = "Gratulace, právě jsi spustila tuto funkci!";
+  ctverecek.style.width = "fit-content";
 }
